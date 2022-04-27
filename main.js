d156ee10cc6b597bf1c09dc2d9f6e74027dfe0ab
Richardhmcosta <101367007+Richardhmcosta@users.noreply.github.com>
2022-04-27 20:49:49 -0300

    build

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 const { app, BrowserWindow } = require('electron')
 
 //Importando a padronização de caminhos
-const path = require('path')
+// const path = require('path')
 
 // Responsavel por ler as alterações e dar um reload na aplicação
-require("electron-reload")(__dirname, {
-    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-})
+// require("electron-reload")(__dirname, {
+//     electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+// })
 // Função responsavel por criar a janela //
 const createWindow = () => {
     const appIconPath = __dirname+'/assets/logozinha.png';
