1cd94e4d3db17ba5c8fc95b572a9f3a4d865be68
Richardhmcosta <101367007+Richardhmcosta@users.noreply.github.com>
2022-04-26 21:41:52 -0300

    Adicionando icone de remover

--- todo-list.js
+++ todo-list.js
@@ -22,7 +22,7 @@ function adicionarItem(event) {
 // Criando a tag LI com JavaScript
     const CriarTagLI = document.createElement('li');
 
-    const tagRemover = "<b onclick='removerItem(event)' > Remover </b>"
+    const tagRemover = `<i onclick='removerItem(event)' class='fa-solid fa-circle-minus icon'> </i>`
 
     // Adicionando um texto dentro da tag LI
   CriarTagLI.innerHTML = valorinput + tagRemover
@@ -31,15 +31,20 @@ function adicionarItem(event) {
 
     minhaTagUl.appendChild(CriarTagLI)
     
-    console.log("Entrou na funçõa")
+    console.log("Entrou na função")
 }
 
 
 
 function removerItem(event) {
-// Adicionando a tag <b> dentro do nosso evento
-  const tagRemover = event.target
-  console.log("Cliquei na função remover")
 
-  console.log(tagRemover)
+// Pegando a tag <i> dentro do nosso evento
+    const meuIcone = event.target
+
+ // Pegando a tag <li> através da tag <i> com propriedade parentElement (tbm pode ser usado parentNode)
+    const minhaLI = meuIcone.parentElement
+
+  // Excluindo a tag <li> com a função remove()
+    minhaLI.remove()
+
 }
